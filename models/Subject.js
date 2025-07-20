5c2361fa528c9c0fb5f6fcf1a8e8631a5397d2ee
satyambhandari88 <satyambhandari7247@gmail.com>
2025-07-20 13:10:13 +0530

    Update Subject.js

--- models/Subject.js
+++ models/Subject.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const subjectSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  code: { type: String, required: true, unique: true }
+  name: { type: String, required: true }
+  
 });
 
 module.exports = mongoose.model('Subject', subjectSchema);
