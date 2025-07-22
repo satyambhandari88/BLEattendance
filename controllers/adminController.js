afa4332a79c0c86cff52418b79c20cc47de7821c
satyambhandari88 <satyambhandari7247@gmail.com>
2025-07-22 13:18:05 +0530

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1236,7 +1236,7 @@ exports.generateSubjectWiseReport = async (req, res) => {
     const doc = new PDFDocument({ 
       margin: 20, // Reduced margins
       size: 'A4', 
-      layout: 'landscape',
+      
       compress: true,
       bufferPages: true // Enable page buffering for better memory management
     });
