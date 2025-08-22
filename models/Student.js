d306c686c01b1a86314eaf025e94aa842557d082
satyambhandari88 <satyambhandari7247@gmail.com>
2025-08-22 12:45:42 +0530

    Update Student.js

--- models/Student.js
+++ models/Student.js
@@ -9,7 +9,12 @@ const StudentSchema = new mongoose.Schema({
   department: { type: String, required: true },
   deviceId: { type: String, default: null },
   year: { type: Number, required: true },
-  faceData: { type: String }
+  faceEnrolled: { type: Boolean, default: false },
+  faceEmbedding: { type: String, default: null },
+  faceEnrollmentDate: { type: Date, default: null },
+  livenessSteps: { type: [Object], default: [] }
 }, { timestamps: true });
+
+
 
 module.exports = mongoose.model('Student', StudentSchema);
